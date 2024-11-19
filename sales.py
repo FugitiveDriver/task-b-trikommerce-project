24134602cb0f80c0ab659c5151cae398280bc106
FugitiveDriver <oliverabbas@gmail.com>
2024-11-19 17:12:55 +0000

    sales completed

--- sales.py
+++ sales.py
@@ -1,6 +1,11 @@
 import random
 
 def daily_sales(available_items, inventory_records, current_day):
+    if current_day % 7 != 0:
+        sold_units = round(random.uniform(0,200))
+        available_items = available_items - sold_units 
+        inventory_records.append([current_day,sold_units,0, available_items]) 
+
     '''
 ***********COMPLETE THIS FUNCTION***********
 This function is responsible for updating the sales for a given day.
